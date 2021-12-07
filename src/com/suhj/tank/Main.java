b792de0dc27f0787abcccb78d61152f03a83bd00
suhj622 <haojiesu@126.com>
2021-12-07 10:54:05 +0800

    添加了配置文件，让程序参数修改更灵活

--- src/com/suhj/tank/Main.java
+++ src/com/suhj/tank/Main.java
@@ -7,8 +7,11 @@ public class Main {
 		
 		TankFrame tf = new TankFrame();
 		
+		//获取敌方坦克的数量
+		int initTankCount =  Integer.parseInt((String)PropertyMgr.get("initTankCount"));
+		
 		//初始化敌方坦克
-		for(int i = 0; i < 5; i++) {
+		for(int i = 0; i < initTankCount; i++) {
 			tf.tanks.add(new Tank(50 + i*80, 200, Dir.DOWN, Group.BAD, tf));
 		}
 		
---
+++ src/com/suhj/tank/PropertyMgr.java
@@ -0,0 +1,35 @@
+package com.suhj.tank;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * 
+ * @author Haojie
+ * 
+ * 2021-12-07 初始化配置文件类
+ * 获取文件中配置好的属性值
+ */
+public class PropertyMgr {
+	
+	static Properties props = new Properties();
+	
+	static {
+		try {
+			props.load(PropertyMgr.class.getClassLoader().getResourceAsStream("config"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object get(String key) {
+		if (props == null) return null;
+		return props.get(key);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(PropertyMgr.get("initTankCount"));
+	}
+
+}
